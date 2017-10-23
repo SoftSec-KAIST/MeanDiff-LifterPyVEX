f9fdf6799bc49de7f6179b4bba9f1f38074f91e0
Markus Faerevaag <m@frv.ag>
2017-10-23 20:31:42 +0900

    Remove build command as its not needed

--- build_src.sh
+++ build_src.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -v $(pwd):/src lifter_pyvex:latest /bin/bash -c 'eval $(opam config env) && cd /src && make'
+# sudo docker run -v $(pwd):/src lifter_pyvex:latest /bin/bash -c ''
